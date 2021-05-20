800bfe3d2b53a2d773812177ff5bce9b93837a28
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-20 09:28:45 +0200

    Boite de dialogue qui demande nom, prénom, ville et les affiche

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var nom = prompt("Quel est votre nom ?");
+var prenom = prompt("Quel est votre prénom ?");
+var ville = prompt("Quelle ville habitez-vous ?");
+
+alert(
+    "Nom : " + nom + "\n" +
+    "Prénom : " + prenom + "\n" +
+    "Ville : " + ville
+)
